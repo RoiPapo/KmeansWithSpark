e18456bff9bac3e1c2f0c5397288160dca13ca82
Roi <thedarkhermit@gmail.com>
2021-05-19 19:54:43 +0300

    Perfectly working leave q out

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from pyspark.sql import SparkSession
 import numpy as np
 from pyspark.sql.functions import lit, monotonically_increasing_id, col, array, udf, avg
 from pyspark.sql import functions as F
-from pyspark.sql.types import StringType, FloatType, IntegerType
+from pyspark.sql import SparkSession
 
-# import org.apache.spark.sql._
 
 
 findspark.init()
@@ -15,12 +14,13 @@ findspark.init()
 
 
 def kmeans_fit(data, k, max_iter, q, init):
+    spark, sc = init_spark('RoiNYarin2')
     centroids = np.array(init)
     for j in range(max_iter):
         prev_cent = centroids
+        centroids  = runer(data, k, max_iter, q, centroids,spark)
         print(np.linalg.norm(np.array(prev_cent) - np.array(centroids)))
-        centroids , tagged_data = runer(data, k, max_iter, q, centroids)
-        if np.linalg.norm(np.array(prev_cent) - np.array(centroids)) < 0.0001:
+        if np.linalg.norm(np.array(prev_cent) - np.array(centroids)) < 0.0005:
             return centroids
     return centroids
 
@@ -31,7 +31,7 @@ def init_spark(app_name: str):
     return spark, sc
 
 
-def runer(data, k, max_iter, q, init):
+def runer(data, k, max_iter, q, init,spark):
     centroids = init
     tagged_data = data.withColumn("id", monotonically_increasing_id()).withColumn("cluster_id", lit(0)).withColumn(
         "dist_from_cent", lit(float('inf')))
@@ -45,13 +45,24 @@ def runer(data, k, max_iter, q, init):
     tagged_data = tagged_data.withColumn('dist', split_col.getItem(0).cast("double")).withColumn('Cluster_num',
                                                                                                  split_col.getItem(
                                                                                                      1).cast("integer"))
-    tagged_data = tagged_data.orderBy(col("Cluster_num").asc(), col("dist").asc())
+    # tagged_data = tagged_data.orderBy(col("Cluster_num").asc(), col("dist").asc())
+    tagged_data.createOrReplaceTempView("DOTS")
+    tagged_data = spark.sql("""WITH added_dense_rank AS (
+  SELECT
+    *,
+    ROW_NUMBER() OVER(PARTITION BY Cluster_num ORDER BY dist ASC) AS rank
+  FROM DOTS
+)
+SELECT
+  *
+FROM added_dense_rank
+WHERE rank %{}!=0 ;""".format(q))
     centroidsTemp = tagged_data.groupBy('Cluster_num').agg(avg("_1").alias("_1"), avg("_2").alias("_2"),
                                                            avg("_3").alias("_3"))
     centroids = np.array(centroidsTemp.select('_1', '_2', '_3').collect())
     centroids[[0, 1]] = centroids[[1, 0]]
     print(centroids)
-    return centroids, tagged_data
+    return centroids
 
 
 def main():
@@ -61,7 +72,7 @@ def main():
     k = 2
     init = data_file.rdd.take(2)
     # df = pd.read_parquet("random_data.parquet")
-    kmeans_fit(data_file, 2, k, 2, init)
+    kmeans_fit(data_file, 2, 10, 12, init)
 
 
 if __name__ == "__main__":
